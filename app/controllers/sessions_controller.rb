243a36097a64365ed23190c75ec54a5b37d774d2
Edwin Pratomo <edwin@insep.si>
2014-08-23 21:51:08 +0700

    added sessions/new

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,16 @@
 class SessionsController < ApplicationController
   def new
+    render :layout => false
   end
 
   def create
+    user = User.find_by(username: params[:username])
+    if user and user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to users_url
+    else
+      #redirect_to login_url, alert "Invalid user/password combination"
+    end
   end
 
   def destroy
