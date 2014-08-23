603364ff4e8ca2645216b942271e0ce8df760494
Edwin Pratomo <edwin@insep.si>
2014-08-24 03:18:10 +0700

    auth works. group based authz not yet.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  skip_before_action :authorize
+  
   def new
     render :layout => false
   end
@@ -7,12 +9,14 @@ class SessionsController < ApplicationController
     user = User.find_by(username: params[:username])
     if user and user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to users_url
+      redirect_to root_url
     else
-      redirect_to login_url, alert("Invalid user/password combination")
+      redirect_to login_url, alert: "Invalid user/password combination"
     end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to login_url
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+  def current_user
+    if session[:user_id]
+      User.find_by(id: session[:user_id])
+    else
+      nil
+    end
+  end
+  
+  def current_user_fullname
+    if session[:user_id]
+      user = User.find_by(id: session[:user_id])
+      if user
+        user.fullname
+      else
+        nil
+      end
+    else
+      nil
+    end
+  end
 end
