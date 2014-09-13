af8c6a7c5151d9f73eb281803263aae8951a0550
Edwin Pratomo <edwin@insep.si>
2014-09-14 00:24:42 +0700

    timezone config

--- app/helpers/transaction_helper.rb
+++ app/helpers/transaction_helper.rb
@@ -1,9 +1,9 @@
 module TransactionHelper
-  # sanitize?
   def transaction_user username, &blk
+    quoted_username = ActiveRecord::Base.connection.quote(username)
     transaction do
       ActiveRecord::Base.connection.execute('CREATE TEMPORARY TABLE current_app_user(username TEXT) ON COMMIT DROP')
-      ActiveRecord::Base.connection.execute("INSERT INTO current_app_user VALUES('#{username}')")
+      ActiveRecord::Base.connection.execute("INSERT INTO current_app_user VALUES('#{quoted_username}')")
       blk.call
     end
   end
