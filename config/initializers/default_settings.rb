f57bf36097c63d9134d458b3bb555934e87d90a1
Edwin Pratomo <edwin@insep.si>
2014-10-01 14:41:43 +0700

    fixed default value for Setting.holidays

--- config/initializers/default_settings.rb
+++ config/initializers/default_settings.rb
@@ -1,4 +1,4 @@
 Setting.defaults[:change_log_archive_interval] = '4_week'
 # use perl CPAN module Calendar::Indonesia::Holiday to get indonesian holidays
 # run: list-id-holidays --json --year-min=2014 --fields=date --fields=ind_name
-Setting.defaults[:holidays] = [] 
+Setting.defaults[:holidays] = {} 
