e4d577a9dd8f0eef44e91477930a922e2f7a3a03
Edwin Pratomo <edwin@insep.si>
2014-10-25 21:26:19 +0700

    added more default settings

--- config/initializers/default_settings.rb
+++ config/initializers/default_settings.rb
@@ -1,5 +1,7 @@
 Setting.defaults[:change_log_archive_interval] = '4_week'
 Setting.defaults[:director] = 'P. Tri Prasetyo, SDB'
+Setting.defaults[:kasie_ppk_name] = 'Dadang Natsir Jayadiwangsa'
+Setting.defaults[:kasie_ppk_nip] = '19580517 198102 1003'
 # use perl CPAN module Calendar::Indonesia::Holiday to get indonesian holidays
 # run: list-id-holidays --json --year-min=2014 --fields=date --fields=ind_name
 Setting.defaults[:holidays] = {} 
