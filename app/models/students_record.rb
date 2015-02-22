de9fd67e235cc2a6fd655fe25fa11666485f0520
Edwin Pratomo <edwin@insep.si>
2015-02-22 16:30:13 +0700

    added validates_uniqueness_of :pkg

--- app/models/students_record.rb
+++ app/models/students_record.rb
@@ -5,10 +5,12 @@ class StudentsRecord < ActiveRecord::Base
   belongs_to :student
   belongs_to :pkg
 
-  validates_presence_of :pkg_id
+  validates_presence_of :pkg
+  validates_uniqueness_of :pkg
+
   validate :finished_on_cant_be_blank
   validate :started_on_cant_be_before_registration
-
+  
   def finished_on_cant_be_blank
     if self.finished_on.blank? and self.finished_on_changed?
       errors.add(:finished_on, "tidak dapat dihapus setelah diset")
