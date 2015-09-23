57858598659b6eaf0f0649c221df3c6cfedbc8d8
Edwin Pratomo <edwin@insep.si>
2015-09-24 01:01:47 +0700

    minor changes to flash_messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -38,8 +38,9 @@ module ApplicationHelper
   def flash_messages(opts = {})
     flash.each do |msg_type, message|
       concat(
-        content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} fade in", role: "alert") do 
+        content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} fade in", role: "alert", style: "font-size:large") do
           concat content_tag(:button, 'x', class: "close", data: { dismiss: 'alert' })
+          concat content_tag(:strong, msg_type.capitalize + ": ")
           concat message
         end
       )
