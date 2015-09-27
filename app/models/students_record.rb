71e281007e9d91a9a487161d1b6aac8e2cd0e38a
Edwin Pratomo <edwin@insep.si>
2015-09-28 00:15:26 +0700

    added validators for dates on students records

--- app/models/students_record.rb
+++ app/models/students_record.rb
@@ -9,12 +9,42 @@ class StudentsRecord < ActiveRecord::Base
   # validates_uniqueness_of :pkg
 
   validate :finished_on_cant_be_blank
+  validate :finished_on_must_be_after_started_on
+  validate :started_on_for_level_above_1
   validate :started_on_cant_be_before_registration
-  
+
   def finished_on_cant_be_blank
     if self.finished_on.blank? and self.finished_on_changed?
       errors.add(:finished_on, "tidak dapat dihapus setelah diset")
       return false
+    end
+  end
+
+  def finished_on_must_be_after_started_on
+    unless self.finished_on.blank?
+      unless self.finished_on > self.started_on
+        errors.add(:finished_on, "hanya dapat diisi tanggal setelah tanggal mulai")
+        return false
+      end
+    end
+  end
+
+  def started_on_for_level_above_1
+    if self.pkg.level > 1
+      prev_level = self.pkg.level - 1
+      prev_record  = self.student.students_records.joins(:pkg).find_by(
+                       status: :finished,
+                       "pkgs.program_id" => self.pkg.program_id,
+                       "pkgs.level" => prev_level
+                     )
+      unless prev_record
+        errors.add(:started_on, "hanya dapat diisi jika sudah lulus level sebelumnya")
+        return false
+      end
+      unless self.started_on > prev_record.finished_on
+        errors.add(:started_on, "hanya dapat diisi tanggal setelah tanggal lulus level sebelumnya")
+        return false
+      end
     end
   end
 
