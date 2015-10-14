df5a5fafe0dab437f9059f247ae88e91c3ef1093
Edwin Pratomo <edwin@insep.si>
2015-10-14 10:49:08 +0700

    added Pkg#grade_component, Exam#grade_component

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -1,5 +1,7 @@
 class Exam < ActiveRecord::Base
   belongs_to :exam_grade_component, foreign_key: 'grade_component_id'
+  alias_method :grade_component, :exam_grade_component
+
   has_many :grades
 
   belongs_to :pkg
--- app/models/pkg.rb
+++ app/models/pkg.rb
@@ -2,11 +2,15 @@ class Pkg < ActiveRecord::Base
   validates_presence_of :pkg
 
   belongs_to :program
-  belongs_to :course
 
   # :students <= :students_pkgs => :pkgs
   has_many :students_pkgs
   has_many :students, through: :students_pkgs
 
   has_many :exams
+
+  belongs_to :course
+  # add Pkg#grade_component:
+  delegate :pkg_grade_component, :to => :course
+  alias_method :grade_component, :pkg_grade_component
 end
