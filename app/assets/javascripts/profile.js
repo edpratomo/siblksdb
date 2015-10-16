56366475fd33205560bb9c477b71ca2eb2228a22
Edwin Pratomo <edwin@insep.si>
2015-10-17 03:58:09 +0700

    footer changes

---
+++ app/assets/javascripts/profile.js
@@ -0,0 +1,6 @@
+$(function() {
+  $('.gravatar_tip').poshytip({content: 'Jika belum punya gravatar, join gravatar.com untuk membuat'});
+});
+$(document).on('page:change', function() {
+  $('.gravatar_tip').poshytip({content: 'Jika belum punya gravatar, join gravatar.com untuk membuat'});
+});
