2809770299da92f978e5b9e00fb1e0ee088d0f93
Edwin Pratomo <edwin@insep.si>
2016-07-23 14:53:14 +0700

    added password reset

---
+++ app/controllers/password_resets_controller.rb
@@ -0,0 +1,31 @@
+class PasswordResetsController < ApplicationController
+  skip_before_action :authorize
+
+  layout 'password_resets.html.erb'
+
+  def create
+    user = User.find_by_email(params[:email])
+    
+    unless user
+      redirect_to new_password_reset_path, alert: "Tidak ditemukan user dengan alamat email #{params[:email]}"
+    else
+      user.send_password_reset
+      redirect_to new_password_reset_path, :notice => "Email sent with password reset instructions."
+    end
+  end
+
+  def edit
+    @user = User.find_by_password_reset_token!(params[:id])
+  end
+
+  def update
+    @user = User.find_by_password_reset_token!(params[:id])
+    if @user.password_reset_sent_at < 2.hours.ago
+      redirect_to new_password_reset_path, :alert => "Password reset has expired."
+    elsif @user.update_attributes(params[:user])
+      redirect_to root_url, :notice => "Password has been reset."
+    else
+      render :edit
+    end
+  end
+end
