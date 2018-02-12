5722a15a2f4f19d01be965f3fafa1a2f88a611d4
Edwin Pratomo <edwin@insep.si>
2018-02-12 11:17:21 +0700

    added charts for all programs

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,8 @@
 class WelcomeController < ApplicationController
   def index
+    @stats = Program.all.sort_by {|e| e.id}.map {|prg| 
+      [prg.program, Student.get_active_stats_for_program(prg, 6)]
+    }.reject {|e| e[1].values.all? {|cnt| cnt == 0}} # exclude zero students
+    @stats.unshift ["Semua Kursus", Student.get_active_stats(6)]
   end
 end
