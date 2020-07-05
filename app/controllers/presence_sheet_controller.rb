2e13df5562a46346d73cb0493ad256e49e41032f
Edwin Pratomo <edwin@insep.si>
2020-07-05 08:08:51 +0700

    presence_sheet now honors started_on value

--- app/controllers/presence_sheet_controller.rb
+++ app/controllers/presence_sheet_controller.rb
@@ -4,7 +4,7 @@ class PresenceSheetController < ApplicationController
 
   def show
   end
-  
+
   def create
     start_day = if params[:time_range] == "next_week"
       DateTime.now.in_time_zone.to_date.beginning_of_week.advance(:days => 7)
@@ -20,31 +20,34 @@ class PresenceSheetController < ApplicationController
 
     holidays_setting = Setting.holidays
     @holidays = @dates.map {|e| holidays_setting[e.strftime("%Y-%m-%d")]}
-    
+
     @instructor = Instructor.find(params[:instructor])
     @sched = Schedule.find(params[:schedule])
-    
+
     ordered_days = %w(mon tue wed thu fri sat)
 
     @student_vs_day = if @instructor and @sched
       students_pkgs_by_day = @instructor.instructors_schedules.where(schedule: @sched).inject({}) do |m,o|
         m[o.day] = o.students_pkgs.inject({}) do |m1,o1|
-          m1[o1.student.uniq_name] = "#{o1.pkg.pkg} / Lev. #{o1.pkg.level}"
+          sr = o1.student.students_records.find_by(pkg: o1.pkg, finished_on: nil)
+          if @dates[ordered_days.index(o.day)] >= sr.started_on
+            m1[o1.student.uniq_name] = "#{o1.pkg.pkg} / Lev. #{o1.pkg.level}"
+          end
           m1
         end
         m
       end
-      
+
       students = @instructor.students.order(name: :asc).uniq
       students.map do |student|
         student_schedule = ordered_days.map do |day|
           students_pkgs_by_day[day] ||= {}
           students_pkgs_by_day[day][student.uniq_name]
-        end  
+        end
         # Rails.logger.debug("student_name #{student.name} (#{student.uniq_name})")
         [student.uniq_name, *student_schedule]
       end.reject {|e| e[1..6].all? {|e| not e }} # exclude students with empty schedules
-    else 
+    else
       []
     end
 
@@ -52,7 +55,7 @@ class PresenceSheetController < ApplicationController
       if true # @students_pkg.update(students_pkg_params)
         # format.html { redirect_to student_schedule_url(student), notice: 'Schedule was successfully updated.' }
         format.html { render :create }
-        format.pdf { 
+        format.pdf {
           render pdf: %[Absensi_#{@instructor.name.gsub(' ', '_')}_#{start_day.strftime("%d-%m-%Y")}],
                  orientation: 'Landscape',
                  template: 'presence_sheet/create.pdf.erb',
