5291e9a2c237b14a5a11482fb2611c68cd8a4320
Edwin Pratomo <edwin@insep.si>
2022-01-02 19:47:13 +0700

    added instructor.is_destroyable?

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -3,23 +3,31 @@ class Instructor < ActiveRecord::Base
   validates :nick, presence: true
   
   # :instructors <= :programs_instructors => :programs => :pkgs
-  has_many :programs_instructors
+  has_many :programs_instructors, dependent: :destroy
   has_many :programs, through: :programs_instructors
   has_many :pkgs, through: :programs
 
   # :instructors <= :instructors_schedules => :schedules
-  has_many :instructors_schedules
+  has_many :instructors_schedules, dependent: :destroy
   has_many :schedules, through: :instructors_schedules
 
   has_many :students_pkgs_instructors_schedules, through: :instructors_schedules
   has_many :students_pkgs,                       through: :students_pkgs_instructors_schedules
   has_many :students,                            through: :students_pkgs
 
+  has_many :grades
+
   # link instructor to user
   has_one :users_instructor
   has_one :user, through: :users_instructor
 
+  before_destroy :is_destroyable?
+
   def options_for_pkg
     pkgs.map {|e| [ "#{e.pkg} - Level #{e.level}", e.id ] }
+  end
+
+  def is_destroyable?
+    user.nil? and grades.first.nil? and students.first.nil?
   end
 end
