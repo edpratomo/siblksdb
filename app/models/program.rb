e31297aa3df73d87dd5b3262540839f71a8b53d3
Edwin Pratomo <edwin@insep.si>
2022-01-03 21:32:10 +0700

    improved /programs, /schedules UI

--- app/models/program.rb
+++ app/models/program.rb
@@ -5,4 +5,10 @@ class Program < ActiveRecord::Base
   # :instructors <= :programs_instructors => :programs
   has_many :programs_instructors
   has_many :instructors, through: :programs_instructors
+
+  before_destroy :is_destroyable?
+
+  def is_destroyable?
+    courses.first.nil? and instructors.first.nil?
+  end
 end
